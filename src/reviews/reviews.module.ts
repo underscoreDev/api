26ee4e0bb55c827b8145c4557c2be80f5ed4145a
underscoreDev <godswill@zimmerman.team>
2022-10-15 23:22:44 +0100

    CR operations

--- src/reviews/reviews.module.ts
+++ src/reviews/reviews.module.ts
@@ -1,8 +1,8 @@
 import { Module } from "@nestjs/common";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { Review } from "src/reviews/entities/reviews.entity";
 import { ReviewsService } from "src/reviews/reviews.service";
 import { ReviewsController } from "src/reviews/reviews.controller";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { Review } from "./entities/reviews.entity";
 
 @Module({
   controllers: [ReviewsController],
