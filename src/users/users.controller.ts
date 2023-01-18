ab10c4abb5e52adb1400e2b9d081f902e39f6386
Godswilldev <gimmex1@gmail.com>
2023-01-18 22:39:28 +0100

    feat:standard responses

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,6 +1,4 @@
-import { Response } from "express";
 import { ApiTags } from "@nestjs/swagger";
-import { User } from "src/users/entities/user.entity";
 import { UsersService } from "src/users/users.service";
 import { RolesGuard } from "src/auth/guards/role.guard";
 import { UpdateUserDto } from "src/users/dto/update-user.dto";
@@ -15,8 +13,6 @@ import {
   Param,
   Delete,
   ParseUUIDPipe,
-  Res,
-  HttpStatus,
   UseGuards,
 } from "@nestjs/common";
 
@@ -28,15 +24,16 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Get()
-  async findAll(@Res() res: Response): Promise<Response<User[]>> {
+  async findAll() {
     const users = await this.usersService.findAll();
-    return res.status(HttpStatus.OK).json({ status: "success", count: users.length, data: users });
+    return { status: "success", count: users.length, data: users };
   }
 
   @Get(":id")
-  @Roles(Role.Admin, Role.Manager)
-  async findOne(@Request() req: any, @Param("id", new ParseUUIDPipe()) id: string): Promise<User> {
-    return await this.usersService.findOne(id);
+  @Roles(Role.User, Role.Manager)
+  async findOne(@Request() req: any, @Param("id", new ParseUUIDPipe()) id: string) {
+    const user = await this.usersService.findOne(id);
+    return { status: "success", data: user };
   }
 
   @Patch(":id")
